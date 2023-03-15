830e9222fff37aee716c8c715faf7a533d312a12
Assalam o Alaikum <faraz.durrani@mvsc.com>
2023-03-15 05:34:36 -0600

    word is stored!

--- app/src/main/java/com/faraz/dictionary/MainActivity.java
+++ app/src/main/java/com/faraz/dictionary/MainActivity.java
@@ -96,20 +96,7 @@ public class MainActivity extends AppCompatActivity {
 
   private void setStoreWordListener() {
     saveView.setOnClickListener((view) -> {
-      Toast.makeText(this, format("Saved '%s'", originalLookupWord), LENGTH_SHORT).show();
-      RequestFuture<JSONObject> future = RequestFuture.newFuture();
-//      JsonObjectRequest request = new JsonObjectRequest(URL, new JSONObject(), future, future);
-//      requestQueue.add(request);
-//
-//      try {
-//        JSONObject response = future.get(); // this will block
-//      }
-//      catch (InterruptedException e) {
-//        // exception handling
-//      }
-//      catch (ExecutionException e) {
-//        // exception handling
-//      }
+      saveWordInMongo();
     });
   }
 
