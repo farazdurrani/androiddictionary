b24e4e9d7f7955ef46d30f384e63bdedccd2ea91
faraz <faraz.uic@gmail.com>
2025-12-03 08:03:45 -0600

    deleting a word is vastly improved

--- app/src/main/java/com/faraz/dictionary/MainActivity5.java
+++ app/src/main/java/com/faraz/dictionary/MainActivity5.java
@@ -16,7 +16,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import java.util.List;
@@ -46,7 +49,7 @@ public class MainActivity5 extends AppCompatActivity {
     repository = new Repository();
     CompletableFuture.supplyAsync(this::getAllWords).thenAccept(_words -> {
       words = _words;
-      runOnUiThread(() -> listView.setAdapter(new ArrayAdapter<>(context, R.layout.custom_layout, words)));
+      runOnUiThread(() -> listView.setAdapter(new ShowRemindedArrayAdapter(context, R.layout.custom_layout, words)));
     }).thenAccept(ignore -> {
       lookupWord = findViewById(R.id.deleteWordBox);
       lookupWord.setThreshold(1);
@@ -60,7 +63,7 @@ public class MainActivity5 extends AppCompatActivity {
     lookupWord.setOnItemClickListener((adapterViewParent, view, position, id) -> {
       String selectedWord = (String) lookupWord.getAdapter().getItem(position);
       runOnUiThread(() -> {
-        listView.setAdapter(new ArrayAdapter<>(context, R.layout.custom_layout, List.of(selectedWord)));
+        listView.setAdapter(new ShowRemindedArrayAdapter(context, R.layout.custom_layout, List.of(selectedWord)));
         lookupWord.setText(null);
         lookupWord.setHint("Enter a word to delete.");
       });
@@ -73,13 +76,13 @@ public class MainActivity5 extends AppCompatActivity {
       if (actionId == EditorInfo.IME_ACTION_DONE || (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER &&
               event.getAction() == KeyEvent.ACTION_DOWN)) {
         String enteredText = textView.getText().toString();
-        words = ImmutableList.<String>builder().addAll(repository.getWords().stream()
-                .filter(w -> w.startsWith(enteredText)).toList()).build().reverse();
+        words = Optional.of(enteredText).filter(StringUtils::isNotBlank).map(et -> ImmutableList.<String>builder()
+                .addAll(repository.getWords().stream().filter(w -> w.startsWith(et)).toList()).build()
+                .reverse().stream().toList()).orElseGet(() -> Lists.reverse(repository.getWords()));
         // hide the keyboard
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
-        runOnUiThread(() -> listView.setAdapter(new ArrayAdapter<>(context, R.layout.custom_layout, words)));
-        restoreAutocompleteBox();
+        restoreAutocompleteBoxAndListView();
         return true;
       }
       return false;
@@ -97,7 +100,7 @@ public class MainActivity5 extends AppCompatActivity {
                   writeToTextFile(word);
                   deleteFromDb(word);
                   deleteWordFromAutoComplete(word);
-                  restoreAutocompleteBox();
+                  restoreAutocompleteBoxAndListView();
                 } catch (Exception exc) {
                   Optional.of(exc).ifPresent(e -> runOnUiThread(() -> listView.setAdapter(new ArrayAdapter<>(context,
                           R.layout.custom_layout, List.of(word, ExceptionUtils.getStackTrace(e))))));
@@ -108,11 +111,17 @@ public class MainActivity5 extends AppCompatActivity {
     });
   }
 
-  private void restoreAutocompleteBox() {
+  private List<String> setWords(List<String> words) {
+    return Optional.of(words).filter(ObjectUtils::isNotEmpty).orElseGet(() -> Lists.reverse(repository.getWords()));
+  }
+
+  private void restoreAutocompleteBoxAndListView() {
     runOnUiThread(() -> {
-      lookupWord.setAdapter(new ShowRemindedArrayAdapter(this, android.R.layout.select_dialog_item, words));
+      lookupWord.setAdapter(new ShowRemindedArrayAdapter(this, android.R.layout.select_dialog_item,
+              Lists.reverse(repository.getWords())));
       lookupWord.setText(null);
       lookupWord.setHint("Enter a word to delete.");
+      listView.setAdapter(new ShowRemindedArrayAdapter(context, R.layout.custom_layout, setWords(words)));
     });
   }
 
@@ -130,7 +139,7 @@ public class MainActivity5 extends AppCompatActivity {
   private void deleteFromDb(String word) {
     repository.delete(word);
     words = words.stream().filter(w -> !w.equals(word)).toList();
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(context, R.layout.custom_layout, words);
+    ArrayAdapter<String> adapter = new ShowRemindedArrayAdapter(context, R.layout.custom_layout, words);
     runOnUiThread(() -> {
       listView.setAdapter(adapter);
       Toast.makeText(context, String.format("%s has been deleted.", word), Toast.LENGTH_SHORT).show();
